45e1543018ac808676758399fa334ecd015d7b20
Nique1 <dominikasarata@gmail.com>
2024-01-07 21:08:14 +0100

    first commit

---
+++ src/main/java/com/Nique1/harmonyOfEmotions/HarmonyOfEmotionsApplication.java
@@ -0,0 +1,13 @@
+package com.Nique1.harmonyOfEmotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyOfEmotionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyOfEmotionsApplication.class, args);
+	}
+
+}
